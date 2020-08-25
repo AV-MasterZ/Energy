3df8e424322d85b9544624502df3c3e652c4abe9
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-08-25 18:04:46 +0400

    added controller

---
+++ src/main/java/org/zhadaev/energy/controller/EnergyController.java
@@ -0,0 +1,23 @@
+package org.zhadaev.energy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zhadaev.energy.DAO.EnergyDao;
+import org.zhadaev.energy.model.Form;
+
+import java.util.List;
+
+@Controller
+public class EnergyController {
+
+    @Autowired
+    private EnergyDao energyDao;
+
+    @GetMapping("/")
+    @ResponseBody
+    public List<Form> getAllForms() {
+        return energyDao.getAllForms();
+    }
+}
